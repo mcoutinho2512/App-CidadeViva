cf983e6c5abb271f3685e900f7b734a78a0c139d
magnun.coutinho <magnuncoutinho@gmail.com>
2026-01-18 00:51:44 -0300

    Initial Commit

---
+++ Cidade Viva/Cidade_VivaApp.swift
@@ -0,0 +1,17 @@
+//
+//  Cidade_VivaApp.swift
+//  Cidade Viva
+//
+//  Created by Magnun Coutinho on 18/01/26.
+//
+
+import SwiftUI
+
+@main
+struct Cidade_VivaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
